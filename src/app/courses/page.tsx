789531e7e6fa21826f1fce7bd89604eea4813610
mishra06 <mishra.vibhakar7262@gmail.com>
2024-08-14 20:19:56 +0530

    "scrollbar issue solve"

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -24,7 +24,7 @@ function page() {
               </CardItem>
               <CardItem
                 key={course.id}
-                as="p"
+                as="p" 
                 translateZ="60"
                 className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
               >
--- src/components/ui/sticky-scroll-reveal.tsx
+++ src/components/ui/sticky-scroll-reveal.tsx
@@ -19,7 +19,7 @@ export const StickyScroll = ({
   const ref = useRef<any>(null);
   const { scrollYProgress } = useScroll({
     // uncomment line 22 and comment line 23 if you DONT want the overflow container and want to have it change on the entire page scroll
-    // target: ref
+    // target: ref,
     container: ref,
     offset: ["start start", "end start"],
   });
@@ -64,10 +64,10 @@ export const StickyScroll = ({
       animate={{
         backgroundColor: backgroundColors[activeCard % backgroundColors.length],
       }}
-      className="h-[30rem] overflow-y-auto flex justify-between relative space-x-10 rounded-md p-10"
+      className="h-[30rem] overflow-y-auto no-scrollbar flex justify-between relative space-x-10 rounded-md p-10"
       ref={ref}
     >
-      <div className="div relative flex items-start px-4">
+      <div className="div relative flex items-start px-4 ">
         <div className="max-w-2xl">
           {content.map((item, index) => (
             <div key={item.title + index} className="my-20">
@@ -95,7 +95,7 @@ export const StickyScroll = ({
               </motion.p>
             </div>
           ))}
-          {/* <div className="h-40" /> */}
+          <div className="h-40" />
         </div>
       </div>
       <div
